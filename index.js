71f6c38b71e9cdfcb445b8104958d6197ebc2e0e
Jennifer Pazos <jmp521@gmail.com>
2021-04-16 22:27:34 -0500

    commit tree ed7bfa6c544f2684c9590b000213268b0d5ea10c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+     fetch("http://localhost:3000/users", {
+          method: "POST", 
+          headers: {
+            'content-type': 'application/json', 
+            accept: 'application/json' 
+        }, 
+        body: JSON.stringify({ 
+            name: name, 
+            email: email
+        }) 
+    }) 
+        .then(response => response.json()) 
+        .then(response => { 
+            let p = document.createElement("p") 
+            const body = document.querySelector("body") 
+            p.id = response.id 
+            p.innerHTML = response.name 
+            body.append(p) 
+        })
+        .catch()
+    }
